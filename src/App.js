c0b03c01c4481df748b6a3b1928af984c5ea6356
Semvil <yurikh11@gmail.com>
2023-09-13 00:15:14 +0300

    All files for work

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./assets/Facebook.svg";
+
+function App() {
+  return (
+    <div className="bg-primary">
+      App
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
