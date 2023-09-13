e91957371f4abfc5c68844d735f40cb9d7fc294e
IamYan94 <yanyanyan623@gmail.com>
2023-09-13 23:23:45 +0300

    secondCommit-FixLaptop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "./assets/Facebook.svg";
-
+import DashboardSection from "./components/DashboardSection";
 function App() {
   return (
-    <div className="bg-primary">
-      App
-      <img src={logo} alt="logo" />
-    </div>
+    <>
+      <div className="bg-primary">
+        App
+        <img src={logo} alt="logo" />
+      </div>
+      <DashboardSection />
+    </>
   );
 }
 
