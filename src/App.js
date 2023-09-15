b72a42b009320582814ca8a96df5351ecf7367b5
ViktorBed <vbednarskii@gmail.com>
2023-09-15 14:53:53 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./assets/Facebook.svg";
+import NavHero from "./components/NavHero";
 
 function App() {
   return (
-    <div className="bg-primary">
-      App
-      <img src={logo} alt="logo" />
+<div>
+        <NavHero/>
     </div>
   );
 }
--- src/components/test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function test() {
-  return <div>test123</div>;
-}
-
-export default test;
